266d0522d4fc6a122aad2c89568e9b399f57a584
Szymo <91956608+s24720@users.noreply.github.com>
2021-10-23 12:06:55 +0200

    Create s01-div.cpp

---
+++ src/s01-div.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    int a, b;
+
+    std::cout << "Podaj liczbę a" << std::endl;
+    std::cin >> a;
+    std::cout << "Podaj liczbę b" << std::endl;
+    std::cin >> b;
+
+    std::cout << "Wynik dzielenia to: "<< a / b << std::endl;
+
+    return 0;
+}
